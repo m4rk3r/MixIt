7ad6697bca98d9b85d721b621417d89aeb4de9b4
Mark Beasley <mbeasl1@gmail.com>
2019-11-11 19:25:55 -0500

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+## ~ MixIt ~
+A very simple tool for composing a mix from youtube videos.
+
+### How to use:
++ install requirements the first time `pip install -e requirements.txt`
+  + also requires [ffmpeg](https://github.com/ytdl-org/youtube-dl/blob/master/README.md#do-i-need-any-other-programs)
++ create a text file with youtube videos on each line, e.g. `subtle-sounds.txt`:
+
+ ```
+https://www.youtube.com/watch?v=17yWeynOfOI
+https://www.youtube.com/watch?v=s3gge17rub0
+https://www.youtube.com/watch?v=sCzJQRVbWtE
+```
+ + you can also add optional crossfade after a track by adding a comma and the duration in seconds:
+
+     ```
+https://www.youtube.com/watch?v=17yWeynOfOI
+https://www.youtube.com/watch?v=s3gge17rub0,5
+https://www.youtube.com/watch?v=sCzJQRVbWtE
+```
+
++ make it happen `python mixit.py subtle-sounds.txt "~ Subtle Sounds ~" ~/Desktop/cover.jpg`
+  + the command is `python mixit.py  <mix file> <id3 title> <cover art>` both title and cover are optional.
+  + The script will create a directory matching the input text filename, download the videos as mp3s via [youtube_dl](https://github.com/ytdl-org/youtube-dl/) and then concatenate into one final mix matching the input filename.
+
++ 🎶 enjoy 🔊
---
+++ mixit.py
@@ -0,0 +1,78 @@
+from __future__ import unicode_literals
+import re
+import subprocess
+import sys
+import youtube_dl
+
+from os import mkdir, path
+from pydub import AudioSegment
+
+
+FFMPEG = subprocess.check_output(['which', 'ffmpeg']).strip()
+mix_name = path.splitext(sys.argv[1])[0]
+
+opts = {
+    'outtmpl': '{}/%(id)s.%(ext)s'.format(mix_name),
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '320',
+    }],
+}
+
+id3 = {
+    'title': sys.argv[2] if len(sys.argv) > 2 else mix_name
+}
+
+config = {
+    'format': 'mp3',
+    'bitrate': '320k',
+    'tags': id3
+}
+
+if not path.exists(mix_name):
+    mkdir(mix_name)
+
+
+def get_id(s):
+    """Get youtube id e.g. &v=< id >"""
+    return re.search(r'\?v=(\w+)(?:,\d+)?$', s).group(1)
+
+
+def get_crossfade(s):
+    """ return optional crossfade converted from Seconds to Milliseconds"""
+    cf = re.search(r',(\d+)$', s)
+    if cf:
+        return int(float(cf.group(1)) * 1000)
+    return 0
+
+
+with open(sys.argv[1], 'r') as f:
+    songs = f.readlines()
+
+    with youtube_dl.YoutubeDL(opts) as ydl:
+        ydl.download([re.sub(r',\d+$', '', s) for s in songs])
+
+    mix = None
+    cf = 0
+
+    for song in songs:
+        song_file = AudioSegment.from_file(
+            path.join(mix_name, '%s.mp3' % get_id(song)))
+
+        if not mix:
+            mix = song_file
+        else:
+            mix = mix.append(song_file, crossfade=cf)
+
+        # set crossfade for mixing with the next song
+        cf = get_crossfade(song)
+
+    with open('%s.mp3' % mix_name, 'wb') as out:
+        if len(sys.argv) > 3:
+            config['cover'] = sys.argv[3]
+
+        mix.export(out, **config)
+
+    print('done!')

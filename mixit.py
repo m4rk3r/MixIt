f5f01d2d6f5dd1bd54275a46cdec4ae29c007b71
Mark Beasley <mbeasl1@gmail.com>
2020-01-09 10:40:26 -0500

    Add standalone app build

--- mixit.py
+++ mixit.py
@@ -3,18 +3,21 @@ import re
 import sys
 import youtube_dl
 
-from os import mkdir, path
+from os import environ, mkdir, path
 from pydub import AudioSegment
+
+
+environ['PATH'] += ':../Frameworks'
 
 
 def get_id(s):
     """Get youtube id e.g. &v=< id >"""
-    return re.search(r'\?v=([\w\d\-_]+)(?:,\d+)?$', s).group(1)
+    return re.search(r'\?v=([\w\d\-_]+)(?:,[0-9.]+)?$', s).group(1)
 
 
 def get_crossfade(s):
     """ return optional crossfade converted from Seconds to Milliseconds"""
-    cf = re.search(r',(\d+)$', s)
+    cf = re.search(r',([0-9.]+)$', s)
     if cf:
         return int(float(cf.group(1)) * 1000)
     return 0
@@ -26,10 +29,12 @@ def build(in_file=None, title=None, cover=None):
         return
 
     mix_name = path.splitext(in_file)[0] if in_file else path.splitext(sys.argv[1])[0]
+    base_dir = path.dirname(sys.argv[1])
 
     opts = {
         'outtmpl': '{}/%(id)s.%(ext)s'.format(mix_name),
         'format': 'bestaudio/best',
+        'ffmpeg_location': '../Frameworks/ffmpeg',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
@@ -41,7 +46,9 @@ def build(in_file=None, title=None, cover=None):
         'format': 'mp3',
         'bitrate': '320k',
         'tags': {
-            'title': title or (sys.argv[2] if len(sys.argv) > 2 else mix_name)
+            'title': title or (sys.argv[2] if len(sys.argv) > 2 else mix_name),
+            'album': title or (sys.argv[2] if len(sys.argv) > 2 else mix_name),
+            'artist': 'Various Artists'
         }
     }
 
@@ -58,10 +65,12 @@ def build(in_file=None, title=None, cover=None):
 
         mix = None
         cf = 0
+        idx = 0
 
         for song in songs:
+            print('processing song {} of {}'.format(idx + 1, len(songs)))
             song_file = AudioSegment.from_file(
-                path.join(mix_name, '%s.mp3' % get_id(song)))
+                path.join(base_dir, mix_name, '%s.mp3' % get_id(song)))
 
             if not mix:
                 mix = song_file
@@ -70,8 +79,10 @@ def build(in_file=None, title=None, cover=None):
 
             # set crossfade for mixing with the next song
             cf = get_crossfade(song)
+            idx += 1
 
-        with open('%s.mp3' % mix_name, 'wb') as out:
+        print('exporting {}...'.format(config['tags']['title']))
+        with open(path.join(base_dir, '%s.mp3' % mix_name), 'wb') as out:
             if cover or len(sys.argv) > 3:
                 config['cover'] = cover or sys.argv[3]
 
@@ -81,4 +92,8 @@ def build(in_file=None, title=None, cover=None):
 
 
 if __name__ == '__main__':
-    build()
+    try:
+        build()
+    except Exception as e:
+        with open(path.join(path.dirname(sys.argv[1]), 'log.txt'), 'w') as log:
+            log.write('error: {}'.format(e))
